48c7a3efdac6c33779200824d683f112a88b09ff
leeiopd <leeiopd@gmail.com>
2021-08-12 22:55:13 +0900

    210812|패캠 node 올인원 04 src 통합

--- javascript/nodejs/패스트캠퍼스/Node 웹 프로그래밍 올인원 패키지/04_sepuelize/src/04/controllers/index.js
+++
@@ -1,9 +0,0 @@
-// 모든 폴더를 작성
-
-const { Router } = require("express");
-const router = Router();
-
-router.use("/admin", require("./admin"));
-// router.use("/contacts", require("./contacts"));
-
-module.exports = router;

cebc0b061ea0ecf56e86ee2a025224f940e7c782
leeiopd <leeiopd@gmail.com>
2021-08-16 22:47:20 +0900

    210816|패캠 Node 올인원 04_09
    04.sequelize
      09_DB 수정

--- javascript/nodejs/패스트캠퍼스/Node 웹 프로그래밍 올인원 패키지/04_sepuelize/src/controllers/admin/admin.ctrl.js
+++ javascript/nodejs/패스트캠퍼스/Node 웹 프로그래밍 올인원 패키지/04_sepuelize/src/controllers/admin/admin.ctrl.js
@@ -34,3 +34,24 @@ exports.get_products_detail = (req, res) => {
     res.render("admin/detail.html", { product });
   });
 };
+
+exports.get_products_edit = (req, res) => {
+  models.Products.findByPk(req.params.id).then((product) => {
+    res.render("admin/write.html", { product });
+  });
+};
+
+exports.post_products_edit = (req, res) => {
+  models.Products.update(
+    {
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  ).then(() => {
+    res.redirect("/admin/products/detail/" + req.params.id);
+  });
+};

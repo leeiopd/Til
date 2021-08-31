c8fc534dbd173d90848f511297a4407c7a708228
leeiopd <leeiopd@gmail.com>
2021-09-01 00:09:23 +0900

    210901
    07.GraphQL
      03_데이터조회

--- javascript/nodejs/패스트캠퍼스/Node 웹 프로그래밍 올인원 패키지/07_GraphQL/src/index.js
+++ javascript/nodejs/패스트캠퍼스/Node 웹 프로그래밍 올인원 패키지/07_GraphQL/src/index.js
@@ -3,15 +3,36 @@ const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
 const schema = buildSchema(`
-    type Query{
-        hello: String,
-        nodejs: Int
-    }
+	type Product {
+		id: ID!
+		name: String
+		price: Int
+		descrptioin: String
+	}
+  
+	type Query{
+		getProduct( id: ID! ): Product
+	}
 `);
 
+const products = [
+  {
+    id: 1,
+    name: "첫번째 제품",
+    price: 2000,
+    description: "히히히히",
+  },
+  {
+    id: 2,
+    name: "두번째 제품",
+    price: 4000,
+    description: "후후후",
+  },
+];
+
 const root = {
-  hello: () => "hello world",
-  nodejs: () => 20,
+  getProduct: ({ id }) =>
+    products.find((product) => product.id === parseInt(id)),
 };
 
 const app = express();

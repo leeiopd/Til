ba4df86a98d89475af9a9ff92153fb5b49e37b5e
leeiopd <leeiopd@gmail.com>
2021-09-28 23:37:50 +0900

    210928|패캠 Node 올인언 09_04
    09.Docker
      04_컨테이너 순서

--- javascript/nodejs/패스트캠퍼스/Node 웹 프로그래밍 올인원 패키지/09_Docker/src/node/docker-entrypoint.sh
+++ javascript/nodejs/패스트캠퍼스/Node 웹 프로그래밍 올인원 패키지/09_Docker/src/node/docker-entrypoint.sh
@@ -1,10 +1,10 @@
-
 #!/bin/bash
-# 아래 "db_url" 따옴표유지
+
+# 아래 "db_url" 따옴표유지, 공백 X
 mysql_host="db_url"
 mysql_port=3306
 
-# mysql container가 뜨기전엔 시작안함
+# mysql container가 실행되기 전에 동작 X
 while ! nc $mysql_host $mysql_port; do  
   >&2 echo "Mysql is unavailable"
   sleep 1
